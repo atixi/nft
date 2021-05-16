c9108faf37aa855a68ab731221f7563060dc52e5
mrhusseiny <mr.hosainy@gmail.com>
2021-05-16 08:45:51 +0430

    conflict resolved

--- Components/Dropdown.jsx
+++ Components/Dropdown.jsx
@@ -3,7 +3,9 @@ import styles from '../styles/dropdown.module.css';
 export default function(props){
     return(
     <span className={styles.dropdown}>
-        <button className={props.trigger.className} style={{...props.trigger.style,boxSizing:"content-box"}}>{props.trigger.title}</button>
+        <button className={props.trigger.className} style={{...props.trigger.style,boxSizing:"content-box"}}>{props.trigger.title+" "}
+            <svg viewBox="0 0 11 7" fill="none" width="10" height="10" xlmns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M5.00146 6.41431L9.70857 1.7072C10.0991 1.31668 10.0991 0.683511 9.70857 0.292986C9.31805 -0.097538 8.68488 -0.097538 8.29436 0.292986L5.00146 3.58588L1.70857 0.292986C1.31805 -0.097538 0.684882 -0.097538 0.294358 0.292986C-0.0961662 0.68351 -0.0961662 1.31668 0.294358 1.7072L5.00146 6.41431Z" fill="currentColor"></path></svg>
+        </button>
         <label>
             <input type="checkbox"/>
             {props.children}
