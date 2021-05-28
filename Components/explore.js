98c2b4f228846b2f142df118d1f97900353ba190
hekmatnajafi <najafihekmat@gmail.com>
2021-05-28 11:59:26 +0430

    explore top list design is done

--- Components/explore.js
+++ Components/explore.js
@@ -2,24 +2,36 @@ import styles from '/styles/Products.module.css'
 import React from 'react';
 import Products from '/Components/products';
 import Profile from "/Components/profileAvatar";
-import { Dropdown, Menu } from 'antd';
+import { Dropdown, Menu, Switch } from 'antd';
 import styled from "styled-components";
 const FilterAndSort = styled.div`
-border: 1px solid gray;
+border: 1px solid rgb(204, 201, 201);
 height: 44px;
 margin-top: -7px !important;
 border-radius: 20px;
+padding: 10px 15px 10px 15px;
 `;
+const FilterAndSortParent = styled.div`
+`
 const menu = (
     <Menu>
-      <Menu.Item key="0">
-        <a href="https://www.antgroup.com">1st menu item</a>
-      </Menu.Item>
-      <Menu.Item key="1">
-        <a href="https://www.aliyun.com">2nd menu item</a>
-      </Menu.Item>
-      <Menu.Divider />
-      <Menu.Item key="3">3rd menu item</Menu.Item>
+      
+    <Menu.ItemGroup title="Sort by">
+      <Menu.Item>{"Recently added"}</Menu.Item>
+      <Menu.Item>{"Cheapest"}</Menu.Item>
+      <Menu.Item>{"Highest price"}</Menu.Item>
+      <Menu.Item>{"Most Liked"}</Menu.Item>
+      <Menu.Item>2nd menu item</Menu.Item>
+    </Menu.ItemGroup>
+      <Menu.ItemGroup title="Options">
+        <Menu.Item>{"Verified only"} <Switch size="small" defaultChecked /></Menu.Item>
+    </Menu.ItemGroup>
+    </Menu>
+  );
+  const exploreMore = (
+    <Menu>
+      <Menu.Item>{"item 1"}</Menu.Item>
+      <Menu.Item>{"item 2"}</Menu.Item>
     </Menu>
   );
 function Explore() {
@@ -42,17 +54,19 @@ function Explore() {
                             <li>💰 DeFi</li>
                             <li>🤡 Memes</li>
                             <li>🤘 Punks</li>
-                            <li><svg viewBox="0 0 14 4" fill="none" width="13.200000000000001" height="13.200000000000001" xlmns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 2C3.5 2.82843 2.82843 3.5 2 3.5C1.17157 3.5 0.5 2.82843 0.5 2C0.5 1.17157 1.17157 0.5 2 0.5C2.82843 0.5 3.5 1.17157 3.5 2ZM8.5 2C8.5 2.82843 7.82843 3.5 7 3.5C6.17157 3.5 5.5 2.82843 5.5 2C5.5 1.17157 6.17157 0.5 7 0.5C7.82843 0.5 8.5 1.17157 8.5 2ZM11.999 3.5C12.8274 3.5 13.499 2.82843 13.499 2C13.499 1.17157 12.8274 0.5 11.999 0.5C11.1706 0.5 10.499 1.17157 10.499 2C10.499 2.82843 11.1706 3.5 11.999 3.5Z" fill="currentColor"></path></svg></li>
+                            <li>
+                                <Dropdown overlay={exploreMore} placement="bottomCenter"  trigger={['click']}>
+                                <svg viewBox="0 0 14 4" fill="none" width="13.200000000000001" height="13.200000000000001" xlmns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 2C3.5 2.82843 2.82843 3.5 2 3.5C1.17157 3.5 0.5 2.82843 0.5 2C0.5 1.17157 1.17157 0.5 2 0.5C2.82843 0.5 3.5 1.17157 3.5 2ZM8.5 2C8.5 2.82843 7.82843 3.5 7 3.5C6.17157 3.5 5.5 2.82843 5.5 2C5.5 1.17157 6.17157 0.5 7 0.5C7.82843 0.5 8.5 1.17157 8.5 2ZM11.999 3.5C12.8274 3.5 13.499 2.82843 13.499 2C13.499 1.17157 12.8274 0.5 11.999 0.5C11.1706 0.5 10.499 1.17157 10.499 2C10.499 2.82843 11.1706 3.5 11.999 3.5Z" fill="currentColor"></path></svg>
+                                </Dropdown>
+                            </li>
                         </ul>
                     </div>
-                    <FilterAndSort className="pr-3">
-                        <Dropdown overlay={menu} placement="bottomCenter"  trigger={['click']}>
-                            <> 
-                              <svg viewBox="0 0 24 24" fill="none" width="22" height="22" xlmns="http://www.w3.org/2000/svg"><path d="M20 16L12 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6 16L4 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M20 8L18 8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 8L4 8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><circle cx="9" cy="16" r="3" fill="currentColor"></circle><circle cx="15" cy="8" r="3" fill="currentColor"></circle></svg>
+                        <Dropdown overlay={menu} placement="bottomCenter" visible={false}  trigger={['click']}>
+                            <FilterAndSort> 
+                              <svg viewBox="0 0 24 24" fill="none" width="22" height="22" xlmns="http://www.w3.org/2000/svg"><path d="M20 16L12 16" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6 16L4 16" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M20 8L18 8" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 8L4 8" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"></path><circle cx="9" cy="16" r="3" fill="currentColor"></circle><circle cx="15" cy="8" r="3" fill="currentColor"></circle></svg>
                                 <span>{"Filter & Sort"}</span>
-                            </>
+                            </FilterAndSort>
                         </Dropdown>
-                    </FilterAndSort>
                 </div>
                 <Products />
 
