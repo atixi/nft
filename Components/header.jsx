34ee1eb884dadb5b2c63634e8e9708211b9751d4
mohcenparsa <mohcen.parsa@gmail.com>
2021-05-31 20:50:38 +0430

    Web3Provider is implementd

--- Components/header.jsx
+++ Components/header.jsx
@@ -6,19 +6,32 @@ import Image from 'next/image'
 import Search from "./search";
 import Dropdown from "./Dropdown";
 import { Menu } from "antd";
-import { useMetaMask } from "metamask-react";
-import { accountList } from "../Constants/constants";
-export default function Header(props) {
+import { useStoreApi } from "../providers/storeApi";
+import { useWeb3 } from "../providers/getWeb";
+
+export default function Header() {
   const [search, setSearch] = useState(false);
   const [menu, setMenu] = useState(false);
-  const { status, connect, account } = useMetaMask();
-  const [welletAddress, setAccountAddress] = useState(account ? account : accountList[0]);
+  const { address, balance, message, setBalance, setAddress } = useStoreApi();
+  const web3 = useWeb3();
 
-  useEffect(()=>{
-    window.ethereum.on("accountsChanged", function(accounts) {
-      setAccountAddress(accounts[0])
-    });    
-  }, [])
+  const setUserAccount = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+      web3.eth.getAccounts().then((account) => {
+        setAddress(account[0]);
+        setUserBalance(account[0]);
+      });
+    }
+  };
+
+  const setUserBalance = async (fromAddress) => {
+    await web3.eth.getBalance(fromAddress).then((value) => {
+      const credit = web3.utils.fromWei(value, "ether");
+      setBalance(credit);
+    });
+  };
+
   const connectToWallet = ()=>{
     console.log('connect to wallet')
 
@@ -413,6 +426,7 @@ export default function Header(props) {
           Create
         </button>
 
+<<<<<<< HEAD
         {status === "notConnected" ? <button
           className={`${styles.btn} ${styles.btnConnect} d-none d-lg-block` } onClick={connect}
         >
@@ -423,6 +437,21 @@ export default function Header(props) {
           Account <span style={{fontWeight: 'bolder'}}>{account && (account.substring(0, 6)+ 
           "..."+account?.substring(account?.length - 4, account?.length ))}</span> 
         </button>}
+=======
+        {address  ? (
+        <>
+          <p>Account: {<span style={{fontWeight: 'bolder'}}>{address && (address.substring(0, 6)+ 
+          "..."+address?.substring(address?.length - 4, address?.length ))}</span> }</p>
+          {/* <p>Balance: {balance}</p> */}
+        </>
+      ) : null}
+       <button
+          className={`${styles.btn} ${styles.btnConnect} d-none d-lg-block` }
+        onClick={() => setUserAccount()}
+      >
+        Connect To Wallet
+      </button>
+>>>>>>> 167aa46 (Web3Provider is implementd)
 
         <button
           onClick={() => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "antd/dist/antd.css";
-import "../styles/style.css";
-import dynamic from "next/dynamic";
-
-import ReactDOM from "react-dom";
-
-import React, { useEffect, useRef } from "react";
-import { Web3ReactProvider } from "@web3-react/core";
-import { MetaMaskProvider } from "metamask-react";
-
-function getLibrary(provider) {
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
-}
+import React from "react";
+import { Web3Provider } from "../providers/store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  let ref = useRef();
-
-  useEffect(() => {
-    ReactDOM.render(
-      <MetaMaskProvider>
-        <Component {...pageProps} />
-      </MetaMaskProvider>,
-      ref.current
-    );
-  }, []);
-  return <div ref={ref} />;
+  return (
+    <React.StrictMode>
+      <Web3Provider>
+        <Component {...pageProps} />;
+      </Web3Provider>
+    </React.StrictMode>
+  );
 }
 
 export default MyApp;
