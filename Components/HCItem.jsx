0d392415f02d695a0e6fba2d1f435aeb6b9bba76
hekmatnajafi <najafihekmat@gmail.com>
2021-06-01 15:39:20 +0430

    added collection title

--- Components/HCItem.jsx
+++ Components/HCItem.jsx
@@ -10,10 +10,10 @@ export default function HCItem(props){
             </CardImageContainer>
             <CardDescription style={{borderTop:"1px solid #ccc"}}>
                 <ProfileAvatarContainer>
-                    <Profile profile={props.item.profpic} size={64} tick={false} />
+                    <Profile profile={props.item.image_url} size={64} tick={false} />
                 </ProfileAvatarContainer>
                 <CardTitle style={{padding:"0px 10px",marginTop:"-35px"}}>
-                    <span>{props.item.title}</span>
+                    <span>{props.item.name}</span>
                     <span>{props.item.subtitle}</span>
                 </CardTitle>
             </CardDescription>
