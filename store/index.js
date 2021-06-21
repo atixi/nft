c6ee0ff7f96616e8e5f6aa2bf11762ca4130a47a
Ali  Alizada <akbar.alizadeh5@gmail.com>
2021-06-21 00:41:24 -0700

    update redux, add redux persist

--- store/index.js
+++ store/index.js
@@ -1,9 +1,40 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import accountSlice, { setAccountToken } from "./action/accountSlice";
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit'
+import accountSlice from "./action/accountSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import logger from 'redux-logger'
 
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
 const multiReducer = combineReducers({
   account: accountSlice,
 });
-export default configureStore({
-  reducer: multiReducer,
-});
+const persistedReducer = persistReducer(persistConfig, multiReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }).concat(logger),
+  devTools: true
+})
+
+
+export const persistor = persistStore(store)
+
+export default store
