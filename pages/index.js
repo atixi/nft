b3b867b931edad729aab18f60517fbf6070ee49e
hekmatnajafi <najafihekmat@gmail.com>
2021-06-22 14:41:20 +0430

    build error solved

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function Home({ topSellers, liveAuctions, collections, explores }) {
     <>
     <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet"/>
     </Head>
     <MainWrapper>
