7da134bdea35cdab82f2c6a6377c6d40de5b02d6
Ahmad <joya.abn@gmail.com>
2021-07-09 11:50:31 +0430

    top sellers in the main page is ok now

--- Components/slider/slide.jsx
+++ Components/slider/slide.jsx
@@ -19,43 +19,6 @@ export default function Slide(props) {
     }
     fetchingCats();
   }, []);
-  const items = [
-    {
-      image: "/images/1.png",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-    {
-      image: "/images/2.jpg",
-      title: "Video Musical Animation",
-      subtitle: "by Eclictic Method",
-    },
-    {
-      image: "/images/3.gif",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-    {
-      image: "/images/4.gif",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-    {
-      image: "/images/5.png",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-    {
-      image: "/images/6.jpg",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-    {
-      image: "/images/7.gif",
-      title: "AsyncArt",
-      subtitle: "Programmable music",
-    },
-  ];
   return (
     <SliderWrapper>
       {categories.map((v, i) => (
