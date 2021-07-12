6620a8acf23a0552ae3dd453b4f617b32342e39a
hekmatnajafi <najafihekmat@gmail.com>
2021-07-12 08:54:17 +0430

    updated strapi base url to request from heroku

--- Utils/axios.js
+++ Utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const baseURL = process.env.BASE_URL;
+const baseURL = process.env.HEROKU_BASE_URL;
 const request = axios.create({
     // url will be updated and will be received from .env file
-    baseURL: "http://localhost:1337",
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
