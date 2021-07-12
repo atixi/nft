d936937569be4b10e5f4e48bd4baa79e4e250962
mohcenparsa <mohcen.parsa@gmail.com>
2021-07-12 19:47:52 +0430

    nft collection type mismatch corrected

--- Utils/axios.js
+++ Utils/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 const baseURL = process.env.HEROKU_BASE_URL;
 const request = axios.create({
-    // url will be updated and will be received from .env file
-    baseURL: baseURL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  // url will be updated and will be received from .env file
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default request;
--- pages/create/erc721.js
+++ pages/create/erc721.js
@@ -1,123 +1,62 @@
 import React, { useEffect, useState } from "react";
 import styles from "/styles/erc721.module.css";
+import qs from "qs";
 import axios from "axios";
-import { CloudUploadOutlined } from "@ant-design/icons";
 import { Menu, Dropdown, Button, Input, Tooltip, Select } from "antd";
 import { fetch, post } from "/Utils/strapiApi";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { localCols, localCategories } from "/Constants/constants";
 
-const pinataApiKey = "7b316dc9992fd3f371ee";
+const pinataApiKey = "de68bc3ddb8bf7a53749";
 const pinataSecretApiKey =
-  "a282c78214f46a036282b889ad01803f84b52c879747436f5a740d56951f5a0c";
+  "47ea101a80715e023688af759d27f84d8b5eca43b94612cb905735eb398fed55";
 const pinataJwt =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJjYzA0M2Y4Yi0yMDA4LTQwNGMtOTczNC1jMzFmOTBhNmFkMzciLCJlbWFpbCI6Im1vaGNlbi5wYXJzYUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlfSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiN2IzMTZkYzk5OTJmZDNmMzcxZWUiLCJzY29wZWRLZXlTZWNyZXQiOiJhMjgyYzc4MjE0ZjQ2YTAzNjI4MmI4ODlhZDAxODAzZjg0YjUyYzg3OTc0NzQzNmY1YTc0MGQ1Njk1MWY1YTBjIiwiaWF0IjoxNjI1OTUzMzgzfQ.etp-RgF8GENrIQItCqYww_GT7ZrEnJoNx0vbSpbnTOg";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1YzEwN2JkMC1kMjA5LTRlOGYtYWM2MS0zYzliZjM1ZjFjODQiLCJlbWFpbCI6Im1wYXJzYUBnYXRld2F5aWN0cy5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlfSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZGU2OGJjM2RkYjhiZjdhNTM3NDkiLCJzY29wZWRLZXlTZWNyZXQiOiI0N2VhMTAxYTgwNzE1ZTAyMzY4OGFmNzU5ZDI3Zjg0ZDhiNWVjYTQzYjk0NjEyY2I5MDU3MzVlYjM5OGZlZDU1IiwiaWF0IjoxNjI2MDkxNDM5fQ.dQcFRAwxieC16TSZMJElj6_4kskljJpR_WdkqppFWQw";
 
-const initNftData = {
-  tokenId: null,
-  tokenAddress: null,
-  name: null,
-  collections: null,
-  categories: null,
-  previewImage: null,
-  talent: null,
-  metadata: {
-    description: null,
-    image_url: null,
-    external_link: null,
-  },
-};
 const { option } = Select;
+
 const ERC721 = () => {
-  const [collectionMenu, setCollectionMenu] = useState(null);
+  const [collections, setCollections] = useState(localCols);
+  const [categories, setCategories] = useState(localCategories);
   const [selectedCollection, setSelectedCollection] = useState(null);
+  const [selectedCategories, setSelectedCategories] = useState(null);
   const [uploadFileUrl, setUploadFileUrl] = useState(null);
   const [file, setFile] = useState(null);
-  const [isValid, setIsValid] = useState(false);
-  const [collections, setCollections] = useState(null);
-  const [nftData, setNftData] = useState(null);
+  // const [isValid, setIsValid] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-  const [name, setName] = useState("");
-  const [collectionName, setCollectionName] = useState("");
-  const [categories, setCategories] = useState(1);
-  const [talent, setTalen] = useState("");
-  const [externalLink, setExternalLink] = useState("");
-  const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [previewImage, setPreviewImage] = useState("");
+  const nftForm = useFormik({
+    initialValues: {
+      tokenId: "",
+      tokenAddress: "",
+      name: "",
+      collections: 1,
+      categories: 1,
+      talent: "",
+      description: "",
+      external_link: "",
+    },
+    validationSchema: Yup.object({
+      tokenId: Yup.string(),
+      tokenAddress: Yup.string(),
+      name: Yup.string().required(),
+      // collections: Yup.number(),
+      talent: Yup.string(),
+      description: Yup.string(),
+      external_link: Yup.string(),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log("subimting");
+      saveNft(values);
+    },
+  });
 
-  // Create a reference to the hidden file input element
-  const hiddenFileInput = React.useRef(null);
-
-  useEffect(() => {
-    loadCollections();
-  }, []);
-  // Programatically click the hidden file input element
-  // when the Button component is clicked
-  const openFileUpload = (event) => {
-    event.preventDefault();
-    hiddenFileInput.current.click();
-  };
-
-  const saveNft = async () => {
-    // const data = {
-    //   tokenId:
-    //     "103750325357517214391910940774049778145622348743697075275564645855722148462593",
-    //   tokenAddress: "0x495f947276749ce646f68ac8c248420045cb7b5e",
-    //   name: "sailor No.3 corpetto",
-    //   collections: selectedCollection.id.toString(),
-    //   categories: "1",
-    //   previewImage:
-    //     "https://gateway.pinata.cloud/ipfs/QmNSfza75tLLj5wcNfL9GqKLncuhNv61gwJCtj9UxdjESV",
-    //   talent: "1",
-    //   metadata: {
-    //     url: "http://hello.com",
-    //     description: "this is very good nft",
-    //     image_url: "http://ifps/hello.com",
-    //   },
-    // };
-    const response = await post("nfts", data);
-    console.log("response is ", response);
-  };
-
-  const handleCollectionChange = (value) => {
-    const col = collections.filter((item) => item.slug == value)[0];
-    setSelectedCollection(col);
-  };
-  const loadCollections = async () => {
-    const result = await fetch("collections");
-    const data = await result.data;
-    if (data) {
-      setCollections(data);
-    }
-  };
-  const handleFileUpload = (event) => {
-    event.preventDefault();
-    var file = event.target.files[0];
-    if (file) {
-      setFile(file);
-      setUploadFileUrl(URL.createObjectURL(event.target.files[0]));
-      console.log("uploadFileUrl ", uploadFileUrl);
-      setIsValid(true);
-    }
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsValid(false);
-    const data = {
-      tokenId:
-        "103750325357517214391910940774049778145622348743697075275564645855722148462593",
-      tokenAddress: "0x495f947276749ce646f68ac8c248420045cb7b5e",
-      name,
-      collections: selectedCollection.id.toString(),
-      categories: "1",
-      previewImage,
-      talent: 1,
-      metadata: {
-        url: externalLink,
-        description,
-        imageUrl,
-      },
-    };
-    // setNftData();
+  const saveNft = async (values) => {
+    setLoading(true);
+    // setIsValid(false);
+    console.log("savenft");
+    console.log("values", values);
     const pinataData = new FormData();
     pinataData.append("file", file);
 
@@ -134,16 +73,89 @@ const ERC721 = () => {
     if (pinataResult.data) {
       const pintaData = await pinataResult.data;
       console.log("pinta data is ", pintaData);
-      setPreviewImage(
-        "https://gateway.pinata.cloud/ipfs/" + pintaData.IpfsHash
-      );
-      setImageUrl("https://gateway.pinata.cloud/ipfs/" + pintaData.IpfsHash);
-      console.log("nft data is : ", data);
-      const nftResponse = await post("nfts", data);
-      console.log("nftResponse is ", nftResponse);
-    }
+      const image_url =
+        "https://gateway.pinata.cloud/ipfs/" + pintaData.IpfsHash;
+      const preview_image_url =
+        "https://gateway.pinata.cloud/ipfs/" + pintaData.IpfsHash;
 
-    setIsValid(true);
+      const nftData = {
+        tokenId:
+          "103750325357517214391910940774049778145622348743697075275564645855722148462593",
+        tokenAddress: "0x495f947276749ce646f68ac8c248420045cb7b5e",
+        name: values.name,
+        collections: {
+          id: values.collections,
+        },
+        metadata: {
+          external_link: values.external_link,
+          description: values.description,
+          name: values.name,
+          description: values.description,
+          image: image_url,
+          preview_image_url: preview_image_url,
+        },
+      };
+
+      console.log("nft data is ", nftData);
+      const nftResponse = await post(
+        `https://rim-entertainment.herokuapp.com/nfts`,
+        nftData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("nftResponse is ", nftResponse);
+      setLoading(false);
+      nftForm.resetForm();
+      // setIsValid(true);
+    }
+  };
+  const hiddenFileInput = React.useRef(null);
+
+  useEffect(() => {
+    // loadCategories();
+    // loadCollections();
+  }, []);
+
+  const openFileUpload = (event) => {
+    event.preventDefault();
+    console.log("file upload click");
+    hiddenFileInput.current.click();
+  };
+
+  const handleCategoriesChange = (value) => {
+    const cats = categories.filter((item) => item.id == value)[0];
+    setSelectedCategories(cats);
+  };
+  const handleCollectionChange = (value) => {
+    const col = collections.filter((item) => item.slug == value)[0];
+    setSelectedCollection(col);
+  };
+  // const loadCollections = async () => {
+  //   const result = await fetch("collections");
+  //   const data = await result.data;
+  //   console.log("collections are is ", data);
+  //   if (data) {
+  //     setCollections(data);
+  //   }
+  // };
+  const loadCategories = async () => {
+    const categoriesData = await fetch("/categories");
+    const cats = await categoriesData.data;
+    console.log("categories", cats);
+    setCategories(cats);
+  };
+  const handleFileUpload = (event) => {
+    event.preventDefault();
+    console.log("hadnel file uplodafsdlkjasl");
+    var file = event.target.files[0];
+    if (file) {
+      // setIsValid(true);
+      setFile(file);
+      setUploadFileUrl(URL.createObjectURL(event.target.files[0]));
+    }
   };
   return (
     <div className={styles.container}>
@@ -154,7 +166,7 @@ const ERC721 = () => {
           File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG,
           GLB, GLTF. Max size: 40 MB
         </p>
-        <form onSubmit={handleSubmit} className={styles.uploadForm}>
+        <form className={styles.uploadForm} onSubmit={nftForm.handleSubmit}>
           <div className={styles.uploadedFileContainer}>
             {uploadFileUrl ? (
               <div className={styles.imageBox}>
@@ -176,6 +188,8 @@ const ERC721 = () => {
               </div>
             )}
             <input
+              name="file"
+              id="file"
               type="file"
               onChange={handleFileUpload}
               ref={hiddenFileInput}
@@ -184,11 +198,16 @@ const ERC721 = () => {
           </div>
           <div className={styles.nftInputComponent}>
             <h3 className={styles.nftSubHeader}>Name *</h3>
+            {nftForm.touched.name && nftForm.errors.name ? (
+              <div className={styles.nftFormErrors}>{nftForm.errors.name}</div>
+            ) : null}
             <input
+              name="name"
+              id="name"
               placeholder="Item Name"
               className={styles.nftInput}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={nftForm.values.name}
+              onChange={nftForm.handleChange}
             />
           </div>
           <div className={styles.nftInputComponent}>
@@ -197,10 +216,12 @@ const ERC721 = () => {
               {"OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details."}
             </p>
             <input
+              name="external_link"
+              id="external_link"
               placeholder="https://yoursite.ion/item/123"
               className={styles.nftInput}
-              value={externalLink}
-              onChange={(e) => setExternalLink(e.target.value)}
+              value={nftForm.values.external_link}
+              onChange={nftForm.handleChange}
             />
           </div>
           <div className={styles.nftInputComponent}>
@@ -209,10 +230,12 @@ const ERC721 = () => {
               {"The description will be included on the item's detail page underneath its image. Markdown syntax is supported."}
             </p>
             <input
+              name="description"
+              id="description"
               placeholder="Provide a detailed description of your item"
               className={styles.nftInput}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              value={nftForm.values.description}
+              onChange={nftForm.handleChange}
             />
           </div>
           <div className={styles.nftInputComponent}>
@@ -221,27 +244,64 @@ const ERC721 = () => {
               This is the collection where your item will appear. info
             </p>
             {collections && (
-              <Select
-                size={`large`}
+              <select
                 style={{ width: `100%`, height: `40px` }}
-                value={
-                  selectedCollection ? selectedCollection?.collectionName : ""
-                }
-                onChange={handleCollectionChange}
+                name="collections"
+                id="collections"
+                value={nftForm.values.collections}
+                onChange={nftForm.handleChange}
               >
                 {collections.map((item) => (
-                  <Select.Option
-                    value={item.slug}
+                  <option
+                    value={item.id}
                     key={item.id}
                     style={{ height: 50, padding: 10 }}
                   >
                     {item.collectionName}
-                  </Select.Option>
+                  </option>
                 ))}
-              </Select>
+              </select>
             )}
           </div>
-          <button disabled={isValid == false} className={styles.createButton}>
+          <div className={styles.nftInputComponent}>
+            <h3 className={styles.nftSubHeader}> Categories *</h3>
+            <p className={styles.nfgParagraph}>
+              This is the Category where your item will appear. info
+            </p>
+            {categories && (
+              <select
+                style={{ width: `100%`, height: `40px` }}
+                name="categories"
+                id="categories"
+                value={nftForm.values.categories}
+                onChange={nftForm.handleChange}
+              >
+                {categories.map((item) => (
+                  <option
+                    value={item.id}
+                    key={item.id}
+                    style={{ height: 50, padding: 10 }}
+                  >
+                    {item.categoryName}
+                  </option>
+                ))}
+              </select>
+            )}
+          </div>
+          <button
+            disabled={!nftForm.isValid || isLoading == true}
+            type="submit"
+            className={styles.createButton}
+            style={
+              !nftForm.isValid || isLoading == true
+                ? {
+                    backgroundColor: `rgba(0,102,255,0.15)`,
+                    color: "black",
+                    border: "none",
+                  }
+                : {}
+            }
+          >
             Create
           </button>
         </form>
