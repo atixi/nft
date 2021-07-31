bc1862835ec48f768041e6941290b247434f9320
hekmatnajafi <najafihekmat@gmail.com>
2021-07-31 13:07:03 +0430

    changed url

--- Utils/axios.js
+++ Utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:1337";
+const baseURL = process.env.HEROKU_BASE_URL;
 const request = axios.create({
   // url will be updated and will be received from .env file
   baseURL: baseURL,
