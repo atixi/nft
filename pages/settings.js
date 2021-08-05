c8ec0ee888edb396d8208d46dc08a25eb502d006
Ahmad <joya.abn@gmail.com>
2021-08-05 13:17:44 +0430

    design is fit

--- pages/settings.js
+++ pages/settings.js
@@ -5,7 +5,7 @@ import CollectionLoader from "@/components/collectionLoader";
 import { useRouter } from "next/router";
 import { ProfileContainer } from "/Components/StyledComponents/talentPage-styledComponents";
 import api from "/Components/axiosRequest";
-import { Form, Input, Button, Radio, Upload } from "antd";
+import { Form, Input, Button, Radio, Upload, Col, Row } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 const normFile = (e) => {
   console.log("Upload event:", e);
@@ -18,20 +18,17 @@ const normFile = (e) => {
 };
 function Profile() {
   const [form] = Form.useForm();
-
   const [isLoad, setLoad] = useState(false);
   const router = useRouter();
   const { profile } = router.query;
 
   const onFinish = async (values) => {
-    console.log("Received values of form: ", values);
     const data = { talentName: values.talentName, bio: values.bio };
     const formData = new FormData();
     formData.append("data", JSON.stringify(data));
     formData.append("files.talentAvatar", values.talentAvatar[0].originFileObj);
     formData.append("files.talentBanner", values.talentBanner[0].originFileObj);
     const req = await api.put(`/talents/${`1`}`, formData);
-    console.log("comes from", req);
   };
 
   useEffect(() => {
@@ -47,53 +44,59 @@ function Profile() {
       <MainWrapper>
         {isLoad === false ? <CollectionLoader /> : ""}
         {isLoad ? (
-          <div>
-            <h5>Edit Profile</h5>
-            <div>You can set prefered display name, username and...</div>
-            <Form form={form} onFinish={onFinish} layout="vertical">
-              <Form.Item
-                label="Name"
-                required
-                name="talentName"
-                rules={[
-                  {
-                    required: true,
-                    message: "talent name is required",
-                  },
-                ]}
-              >
-                <Input placeholder="write your name" />
-              </Form.Item>
-              <Form.Item label="Bio" name="bio">
-                <Input placeholder="write about your self" />
-              </Form.Item>
-              <Form.Item
-                name="talentAvatar"
-                label="profile picture or avatar"
-                extra="use square image"
-                valuePropName="fileList"
-                getValueFromEvent={normFile}
-              >
-                <Upload listType="picture" maxCount={1}>
-                  <Button icon={<UploadOutlined />}>Click to upload</Button>
-                </Upload>
-              </Form.Item>
-              <Form.Item
-                name="talentBanner"
-                label="cover page"
-                valuePropName="fileList"
-                getValueFromEvent={normFile}
-              >
-                <Upload listType="picture" maxCount={1}>
-                  <Button icon={<UploadOutlined />}>Click to upload</Button>
-                </Upload>
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
+          <div className="mt-4">
+            <Row>
+              <Col md={6}></Col>
+              <Col md={12}>
+                <h5>Edit Profile</h5>
+                <div>You can set prefered display name, username and...</div>
+                <Form form={form} onFinish={onFinish} layout="vertical">
+                  <Form.Item
+                    label="Name"
+                    required
+                    name="talentName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "talent name is required",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="write your name" />
+                  </Form.Item>
+                  <Form.Item label="Bio" name="bio">
+                    <Input placeholder="write about your self" />
+                  </Form.Item>
+                  <Form.Item
+                    name="talentAvatar"
+                    label="profile picture or avatar"
+                    extra="use square image"
+                    valuePropName="fileList"
+                    getValueFromEvent={normFile}
+                  >
+                    <Upload listType="picture" maxCount={1}>
+                      <Button icon={<UploadOutlined />}>Click to upload</Button>
+                    </Upload>
+                  </Form.Item>
+                  <Form.Item
+                    name="talentBanner"
+                    label="cover page"
+                    valuePropName="fileList"
+                    getValueFromEvent={normFile}
+                  >
+                    <Upload listType="picture" maxCount={1}>
+                      <Button icon={<UploadOutlined />}>Click to upload</Button>
+                    </Upload>
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+              <Col md={6}></Col>
+            </Row>
           </div>
         ) : (
           ""
