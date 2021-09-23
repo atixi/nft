d6567baf14ac61aa232675eb06c6dda6972236d6
hekmatnajafi <najafihekmat@gmail.com>
2021-09-23 12:43:55 +0430

    add responsiveness to hotCollecttion

--- Components/explore.js
+++ Components/explore.js
@@ -28,6 +28,20 @@ function Explore() {
     dataLoad: true,
     dataLoadMoreButtonLoading: false,
   });
+  const responsive ={
+    0:{
+        items:1
+    },
+    400:{
+        items: 2
+    },
+    600:{
+        items:3
+    },
+    1000:{
+        items:5
+    }
+    }
   async function LoadMoreData(slug) {
     setLoadMore({
       ...loadMore,
@@ -346,7 +360,7 @@ function Explore() {
                                 <div className="col-lg-12">
                                     <h2 className="style-2">Hot Collections</h2>
                                 </div>
-                                <OwlCarousel className='owl-theme  fadeIn' dots={false} items={5} navClass={["owl-prev carouselPrev", "owl-next carouselNext"]} loop margin={15} nav>
+                                <OwlCarousel className='owl-theme  fadeIn' responsive={responsive} responsiveClass={"-"} dots={false} items={5} navClass={["owl-prev carouselPrev", "owl-next carouselNext"]} loop margin={15} nav>
 
                                         <div className="nft_coll style-2">
                                             <div className="nft_wrap">
