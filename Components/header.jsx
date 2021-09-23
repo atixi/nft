286b84307aa4f16f01168edd0e977159d0b47810
hekmatnajafi <najafihekmat@gmail.com>
2021-09-23 21:30:19 +0430

    cleanup

--- Components/header.jsx
+++ Components/header.jsx
@@ -131,27 +131,27 @@ function Header(props) {
     );
   };
   return (
-    <header class="transparent header-light scroll-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="de-flex sm-pt10">
-                    <div class="de-flex-col">
-                        <div class="de-flex-col">
+    <header className="transparent header-light scroll-light">
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="de-flex sm-pt10">
+                    <div className="de-flex-col">
+                        <div className="de-flex-col">
                             {/* <!-- logo begin --> */}
                             <div id="logo">
                                 <a href="index.html">
-                                    <img alt="" class="logo" src="images/logo-light.png" />
-                                    <img alt="" class="logo-2" src="images/logo.png" />
+                                    <img alt="" className="logo" src="images/logo-light.png" />
+                                    <img alt="" className="logo-2" src="images/logo.png" />
                                 </a>
                             </div>
                             {/* <!-- logo close --> */}
                         </div>
-                        <div class="de-flex-col">
-                            <input id="quick_search" class="xs-hide" name="quick_search" placeholder="search item here..." type="text" />
+                        <div className="de-flex-col">
+                            <input id="quick_search" className="xs-hide" name="quick_search" placeholder="search item here..." type="text" />
                         </div>
                     </div>
-                    <div class="de-flex-col header-col-mid">
+                    <div className="de-flex-col header-col-mid">
                         {/* <!-- mainmenu begin --> */}
                         <ul id="mainmenu">
                             <li>
@@ -213,8 +213,8 @@ function Header(props) {
                                 </ul>
                             </li>
                         </ul>
-                        <div class="menu_side_area">
-                            <a href="wallet.html" class="btn-main"><i class="icon_wallet_alt"></i><span>Connect Wallet</span></a>
+                        <div className="menu_side_area">
+                            <a href="wallet.html" className="btn-main"><i className="icon_wallet_alt"></i><span>Connect Wallet</span></a>
                             <span id="menu-btn"></span>
                         </div>
                     </div>
