def2b0d6d92eaa064368e85030855dc3a963dae0
hekmatnajafi <najafihekmat@gmail.com>
2021-10-04 00:46:08 +0430

    updated slide link

--- Components/slider/slide.jsx
+++ Components/slider/slide.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import request from "../../Utils/axios";
 import dynamic from "next/dynamic"
+import Link from "next/link"
 const OwlCarousel = dynamic(import("react-owl-carousel"), { ssr: false });
 
 function Slide() {
@@ -25,15 +26,14 @@ function Slide() {
         <section id="section-hero " className="no-bottom topCarouselSection" aria-label="section">
             <div className="d-carousel">
                 {assets && assets?.length > 0 && <OwlCarousel margin={15} dots={false} id="item-carousel-big" className="owl-carousel" navClass={["owl-prev topCarouselPrev", "owl-next topCarouselNext"]} nav>
-
                     {assets.map((asset) => {
                         return <div className="nft_pic">
-                            <a href="item-details.html">
+                            <Link href={`/nft/${asset?.tokenAddress}?tokenId=${asset?.tokenId}`}><a>
                                 <span className="nft_pic_info">
                                     <span className="nft_pic_title">{asset.asset?.collection?.name}</span>
                                     <span className="nft_pic_by">{asset.asset?.name}</span>
                                 </span>
-                            </a>
+                            </a></Link>
                             <div className="nft_pic_wrap">
                                 <img src={asset.asset?.imageUrlOriginal} className="lazy img-fluid" alt="" />
                             </div>
