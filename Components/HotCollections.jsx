f56b742d62e1cd173bc1131737d804895bdc0bc4
hekmatnajafi <najafihekmat@gmail.com>
2021-10-04 22:19:36 +0430

    refactored hot collection

--- Components/HotCollections.jsx
+++ Components/HotCollections.jsx
@@ -25,6 +25,7 @@ function HotCollections() {
         try {
             const collections = await fetch("/collections");
             if (collections) {
+                console.log("hot collections", collections)
                 setServerCollections(collections.data);
             }
         }
@@ -45,19 +46,19 @@ function HotCollections() {
             <div className="col-lg-12">
                 <h2 className="style-2">Hot Collections</h2>
             </div>
-            {serverCollections && <OwlCarousel className='owl-theme  fadeIn' responsive={responsive} responsiveClass={"-"} dots={false} items={5} navClass={["owl-prev carouselPrev", "owl-next carouselNext"]} loop margin={15} nav>
+            {serverCollections && <OwlCarousel className='owl-theme  fadeIn' responsive={responsive} responsiveClass={"-"} dots={false} items={5} navClass={["owl-prev carouselPrev", "owl-next carouselNext"]} loop={false} margin={15} nav>
                 {serverCollections.map((collection, index) => {
                     return <div key={index} className="nft_coll style-2">
                         <div className="nft_wrap">
                             <a href="collection.html"><img src={collection?.collectionBanner?.url} className="lazy img-fluid" alt="" /></a>
                         </div>
                         <div className="nft_coll_pp">
-                            <a href="collection.html"><img className="lazy" src={collection?.talent?.talentAvatar?.url} alt="" /></a>
+                            <a href="collection.html"><img className="lazy" src={collection?.collectionImageURL?.url} alt="" /></a>
                             <i className="fa fa-check"></i>
                         </div>
                         <div className="nft_coll_info">
                             <a href="collection.html"><h4>{collection?.collectionName}</h4></a>
-                            <span>ERC-192</span>
+                            <span>{`${collection?.nfts?.length} assets`}</span>
                         </div>
                     </div>
                 })}
