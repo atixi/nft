ede6a957e272836b9906327b3354f41b929e603a
hekmatnajafi <najafihekmat@gmail.com>
2021-10-07 00:52:09 +0430

    added error message to login and redirect link

--- Components/loginModal.js
+++ Components/loginModal.js
@@ -1,20 +1,24 @@
 import react from "react"
-import { Modal, Form } from "antd"
+import { Modal, Form, notification } from "antd"
 import { signin } from '../store/action/accountSlice';
 import { useDispatch } from 'react-redux';
+import { useRouter } from "next/router"
 function LoginModal({ showLoginModal, setShowLoginModal }) {
 
     const dispatch = useDispatch();
-
+    const router = useRouter()
     const onFinish = async (values) => {
-        console.log("submitting")
         try {
             const res = await dispatch(signin(values));
             if (res.meta?.requestStatus === "fulfilled") {
                 setShowLoginModal(false)
+                router.push("/")
             }
             else if (res.meta?.requestStatus === "rejected") {
-                console.log("username or password is wrong")
+                notification["error"]({
+                    duration: 5,
+                    message: "Username or Password is wrong",
+                });
             }
         } catch (err) {
             console.log('ERR:Login:onFinish ', err);
@@ -28,7 +32,7 @@ function LoginModal({ showLoginModal, setShowLoginModal }) {
         <Form onFinish={onFinish} name="contactForm" id='contact_form' className="form-border" method="post" action='blank.php'>
             <h3>Login to your account</h3>
             <div className="field-set">
-                <label>Username</label>
+                <label>Email</label>
                 <Form.Item name={"username"} rules={[
                     {
                         required: true,
@@ -51,10 +55,7 @@ function LoginModal({ showLoginModal, setShowLoginModal }) {
             </div>
             <div id='submit'>
                 <input type='submit' id='send_message' value='Login' className="btn btn-main color-2" />
-                <div id='mail_success' class='success'>Your message has been sent successfully.</div>
-                <div id='mail_fail' class='error'>Sorry, error occured this time sending your message.</div>
-                <div className="clearfix"></div>
-                <div className="spacer-single"></div>
+
             </div>
         </Form>
     </Modal>)
