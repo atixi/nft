16938af692491448c78fd8bd0f6b1ff0596a5938
Nekfar Baqir <m.nekfar.baqir.786@gmail.com>
2021-10-10 10:49:21 +0430

    the build error solved

--- pages/wallet.js
+++ pages/wallet.js
@@ -125,68 +125,68 @@ const Wallet = () => {
         </div>
       </section>
       <section aria-label="section">
-        <div class="contentContainer">
-          <div class="row">
-            <div class="col-lg-3 mb30">
-              <a class="box-url" onClick={onDesktopConnect}>
-                <span class="box-url-label">Most Popular</span>
-                <img src="images/wallet/1.png" alt="" class="mb20" />
+        <div className="contentContainer">
+          <div className="row">
+            <div className="col-lg-3 mb30">
+              <a className="box-url" onClick={onDesktopConnect}>
+                <span className="box-url-label">Most Popular</span>
+                <img src="images/wallet/1.png" alt="" className="mb20" />
                 <h4>Metamask</h4>
                 <p>Start exploring blockchain applications in seconds.  Trusted by over 1 million users worldwide.</p>
               </a>
             </div>
-            <div class="col-lg-3 mb30">
-              <a class="box-url" aria-disabled={true} href={null}>
-                <img src="images/wallet/2.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" aria-disabled={true} href={null}>
+                <img src="images/wallet/2.png" alt="" className="mb20" />
                 <h4>Bitski</h4>
                 <p>Bitski connects communities, creators and brands through unique, ownable digital content.</p>
               </a>
             </div>
-            <div class="col-lg-3 mb30 disable">
-              <a class="box-url" href={null}>
-                <img src="images/wallet/3.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30 disable">
+              <a className="box-url" href={null}>
+                <img src="images/wallet/3.png" alt="" className="mb20" />
                 <h4>Fortmatic</h4>
                 <p>Let users access your Ethereum app from anywhere. No more browser extensions.</p>
               </a>
             </div>
 
-            <div class="col-lg-3 mb30">
-              <a class="box-url" href={null}>
-                <img src="images/wallet/4.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" href={null}>
+                <img src="images/wallet/4.png" alt="" className="mb20" />
                 <h4>WalletConnect</h4>
                 <p>Open source protocol for connecting decentralised applications to mobile wallets.</p>
               </a>
             </div>
 
-            <div class="col-lg-3 mb30">
-              <a class="box-url" href={null}>
-                <img src="images/wallet/5.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" href={null}>
+                <img src="images/wallet/5.png" alt="" className="mb20" />
                 <h4>Coinbase Wallet</h4>
                 <p>The easiest and most secure crypto wallet. ... No Coinbase account required.
                                 </p>
               </a>
             </div>
 
-            <div class="col-lg-3 mb30">
-              <a class="box-url" href={null}>
-                <img src="images/wallet/6.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" href={null}>
+                <img src="images/wallet/6.png" alt="" className="mb20" />
                 <h4>Arkane</h4>
                 <p>Make it easy to create blockchain applications with secure wallets solutions.</p>
               </a>
             </div>
 
-            <div class="col-lg-3 mb30">
-              <a class="box-url" href={null}>
-                <img src="images/wallet/7.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" href={null}>
+                <img src="images/wallet/7.png" alt="" className="mb20" />
                 <h4>Authereum</h4>
                 <p>Your wallet where you want it. Log into your favorite dapps with Authereum.</p>
               </a>
             </div>
 
-            <div class="col-lg-3 mb30">
-              <a class="box-url" href={null}>
-                <span class="box-url-label">Most Simple</span>
-                <img src="images/wallet/8.png" alt="" class="mb20" />
+            <div className="col-lg-3 mb30">
+              <a className="box-url" href={null}>
+                <span className="box-url-label">Most Simple</span>
+                <img src="images/wallet/8.png" alt="" className="mb20" />
                 <h4>Torus</h4>
                 <p>Open source protocol for connecting decentralised applications to mobile wallets.</p>
               </a>
