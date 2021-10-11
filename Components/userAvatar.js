d167223add4e97c966d1f354c1c81b07d3969945
hekmatnajafi <najafihekmat@gmail.com>
2021-10-11 18:16:55 +0430

    added scrol to asset details page

--- Components/userAvatar.js
+++ Components/userAvatar.js
@@ -1,13 +1,12 @@
 import react from "react"
 import Link from "next/link"
 function UserAvatar({ user }) {
-    return <div className="author_list_pp">
-        {user.verified ? <Link href={`/profile/${user.userName}`}><a>
+    return <div className="author_list_pp userAvatar">
+        {user.verified ? <Link href={`/profile/${user.userName ? user.userName : user.address}`}><a>
             <img className="lazy" src={user.avatar} alt="user" /><i className="fa fa-check"></i>
         </a></Link> :
             <img className="lazy" src={user.avatar} alt="user" />
         }
     </div>
 }
-
 export default UserAvatar
