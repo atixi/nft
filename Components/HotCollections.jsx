78ff870ea1a0d55389800d7d3736131a44bdf235
hekmatnajafi <najafihekmat@gmail.com>
2021-10-13 00:37:34 +0430

    refined design in collection list

--- Components/HotCollections.jsx
+++ Components/HotCollections.jsx
@@ -50,10 +50,10 @@ function HotCollections({ collections }) {
               <Link href={`/collection/${collection?.slug}`} passHref>
                 <a>
                   <div key={index} className="nft_coll style-2">
-                    <div className="nft_wrap">
+                    <div className="nft_wrap hotCollectionListDiv">
                       <img
                         src={collection?.collectionBanner?.url}
-                        className="lazy img-fluid"
+                        className="lazy img-fluid rounded hotCollectionListImg"
                         alt=""
                       />
                     </div>
