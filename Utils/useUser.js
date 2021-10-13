324494f85e6a46f6ecf02948528ea43216d87bd1
Ahmad <joya.abn@gmail.com>
2021-10-13 10:58:31 +0430

    login url fixed

--- Utils/useUser.js
+++ Utils/useUser.js
@@ -3,25 +3,28 @@ import Router from "next/router";
 import useSWR from "swr";
 
 export default function useUser({
-    redirectTo = false,
-    redirectIfFound = false,
+  redirectTo = false,
+  redirectIfFound = false,
 } = {}) {
-    const fetcher = url => console.log(url) || fetch(url).then(r => r.json())
-    const { data: user, mutate: mutateUser } = useSWR("http://localhost:3000/api/user", fetcher);
-    useEffect(() => {
-        // if no redirect needed, just return (example: already on /dashboard)
-        // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-        if (!redirectTo || !user) return;
+  const fetcher = (url) => console.log(url) || fetch(url).then((r) => r.json());
+  const { data: user, mutate: mutateUser } = useSWR(
+    "https://rim-nft-platform.herokuapp.com/api/user",
+    fetcher
+  );
+  useEffect(() => {
+    // if no redirect needed, just return (example: already on /dashboard)
+    // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
+    if (!redirectTo || !user) return;
 
-        if (
-            // If redirectTo is set, redirect if the user was not found.
-            (redirectTo && !redirectIfFound && !user?.isLoggedIn) ||
-            // If redirectIfFound is also set, redirect if the user was found
-            (redirectIfFound && user?.isLoggedIn)
-        ) {
-            Router.push(redirectTo);
-        }
-    }, [user, redirectIfFound, redirectTo]);
+    if (
+      // If redirectTo is set, redirect if the user was not found.
+      (redirectTo && !redirectIfFound && !user?.isLoggedIn) ||
+      // If redirectIfFound is also set, redirect if the user was found
+      (redirectIfFound && user?.isLoggedIn)
+    ) {
+      Router.push(redirectTo);
+    }
+  }, [user, redirectIfFound, redirectTo]);
 
-    return { user, mutateUser };
+  return { user, mutateUser };
 }
